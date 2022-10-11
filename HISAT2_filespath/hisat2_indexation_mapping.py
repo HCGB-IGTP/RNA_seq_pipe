a0ddbd9f51c6d6535e32c49648172275f28a3497
Jose Francisco Sanchez-Herrero <jfbioinformatics@gmail.com>
2022-10-11 12:02:42 +0200

    Update hisat2_indexation_mapping.py

--- HISAT2_filespath/hisat2_indexation_mapping.py
+++ HISAT2_filespath/hisat2_indexation_mapping.py
@@ -7,71 +7,91 @@ Created on Oct 8, 2022
 import os 
 import sys
 
-def main():
+#########################################################
+def hisat2_index(path_reference, reference, index):
+
+    ## OLD
+    #os.chdir(path_reference) #change to the working directory 
+    #indexing = "hisat2-build "+reference+" "+index #bash command 
     
-    def hisat2_index():
-        
-        path_reference = sys.argv[1] #first argument --> working directory 
-        reference = sys.argv[2]  #second argument--> reference genome name 
-        index = sys.argv[3] #third argument --> index name
-       
-        os.chdir(path_reference) #change to the working directory 
-        
-        indexing = "hisat2-build "+reference+" "+index #bash command 
-        
-        os.system(indexing)
-       
+    ## new variable
+    reference_abs_path = os.path.join(path_reference, reference)
+    index_abs_path = os.path.join(path_reference, index)
+    indexing = "hisat2-build " + reference_abs_path + " " + index_abs_path #bash command 
+
+    os.system(indexing)
     
-    def hisat2_mapping():
-        
+    return(index_abs_path)    
     
-        path_index = sys.argv[1] #index path 
-        index = sys.argv[3] #index name
-        index_join = os.path.join(path_index,index) #creates correct path to index
+
+ #########################################################
+ def hisat2_mapping(index_join, path_reads, path_output):   
     
-        path_reads = sys.argv[4] #reads path
-        path_output = sys.argv[5] #output path 
-        
-        os.chdir(path_reads)
-        
-        reads = os.listdir() #saves files in current directory as a variable 
+    os.chdir(path_reads)
+
+    reads = os.listdir() #saves files in current directory as a variable 
+
+    reads_1 = [] #list with reads forward
+    reads_2 = [] #list with reads reverse  
+
+    """
+    !!!!!!!!!!!!! This loop must be changed when using real named reads !!!!!!!!!!!
+
+    """
     
-        reads_1 = [] #list with reads forward
-        reads_2 = [] #list with reads reverse  
-        
-        """
-        !!!!!!!!!!!!! This loop must be changed when using real named reads !!!!!!!!!!!
-        
-        """
-        
-        for r in reads: 
-            if r.endswith('.fastq'): #extract the reads from all the files in the directory
-                if "1" in r: #reads -1
-                    reads_1.append(r)
-                else: # reads -2
-                    reads_2.append(r) 
-            
-        """
-        """
-        
-        for i,j in zip (reads_1, reads_2): #for every couple of reads we run the hisat2 command
-            forward = i #-1 read
-            reverse = j  # -2 read
-            name=i+j+".sam" #name of output sam 
-            name=name.replace(".fastq", "") #aesthetic name
-            path_sam = os.path.join(path_output,name)
-            
-            mapping = "hisat2 -x"+index_join+" -1"+forward+" -2"+reverse+" -S"+path_sam #hisat2 mapping command 
-            
-            os.system(mapping)
+    ## get reads
+    for r in reads: 
+        if r.endswith('.fastq'): #extract the reads from all the files in the directory
+            if "1" in r: #reads -1
+                reads_1.append(r)
+            else: # reads -2
+                reads_2.append(r) 
+
+    """
+    """
+
+    ## create mapping
+    for i,j in zip (reads_1, reads_2): #for every couple of reads we run the hisat2 command
+        forward = i #-1 read
+        reverse = j  # -2 read
+        name=i+j+".sam" #name of output sam 
+        name=name.replace(".fastq", "") #aesthetic name
+        path_sam = os.path.join(path_output,name)
+
+        mapping = "hisat2 -x"+index_join+" -1"+forward+" -2"+reverse+" -S"+path_sam #hisat2 mapping command 
+
+        os.system(mapping)
    
-    hisat2_index()
-    hisat2_mapping()    
-
-main()
+#########################################################
+def main():  
+    ## sys,argv[0] script
+    ## sys.argv[1] #first argument --> working directory 
+    ## sys.argv[2]  #second argument--> reference genome name 
+    ## sys.argv[3] #third argument --> index name
+    ## sys.argv[4] # reads folder
+    ## sys.argv[5] #output
     
+    ## check length(sys.argv) == 6
+    ## si error: 
+        ## help: 
+        ## python hisat2_indexation_mapping.py /dir/folder/with/reference_folder file_name_reference index_name reads_folder output_folder
+    
+    ## index
+    path_reference = sys.argv[1] #first argument --> working directory 
+    reference = sys.argv[2]  #second argument--> reference genome name 
+    index = sys.argv[3] #third argument --> index name
+    
+    ### call index
+    index_abs_path = hisat2_index(path_reference, reference, index)
+
+    ## map reads
+    path_reads = sys.argv[4] #reads path
+    path_output = sys.argv[5] #output path 
+
+    hisat2_mapping(index_abs_path, path_reads, path_output)
+    
+    
+#########################################################
 if __name__ == '__main__':
-    pass
-
-    
-    
+    main()
+       
