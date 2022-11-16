ea8c10af3835b44362fd3376b5221b719b3ad361
jfsanchezherrero <jfbioinformatics@gmail.com>
2022-11-16 15:33:04 +0100

    remove scripts

--- featurecounts.py
+++
@@ -1,8 +0,0 @@
-import os 
-
-def featurecounts (path_results, sorted_bam_name, threads, gtf):
-
-    featurecounts = "featureCounts -p -O -T " + threads + " -a " + gtf + " -o " + path_results + " " + sorted_bam_name 
-    print(featurecounts)
-    os.system(featurecounts) 
-
