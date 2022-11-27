4222fd68dcb9a7eeb8d9b92d3695cab79518f188
mirgin01 <100416435+mirgin01@users.noreply.github.com>
2022-11-27 18:12:16 +0100

    Update featurecounts.py

--- rnaseqpipe/rnaseqpipe/scripts/featurecounts.py
+++ rnaseqpipe/rnaseqpipe/scripts/featurecounts.py
@@ -2,7 +2,11 @@ import os
 
 def featurecounts (path_results, sorted_bam_name, threads, gtf):
 
-    featurecounts = "featureCounts -p -O -T " + threads + " -a " + gtf + " -o " + path_results + " " + sorted_bam_name 
+    output = os.path.join(path_results, "Counts_Matrix_Output.txt") 
+       
+    featurecounts = "featureCounts -p -O -T " + threads + " -a " + gtf + " -o " + output + " " + sorted_bam_name 
+    
     print(featurecounts)
+    
     os.system(featurecounts) 
 
